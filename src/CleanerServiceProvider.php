d6c239e40bf5013f2cf358824c920ff8049f2423
duyonggen <dyg0924@sina.cn>
2021-03-03 13:35:41 +0800

    first commit.

---
+++ src/CleanerServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Inn20\VendorCleaner;
+
+use Illuminate\Support\ServiceProvider;
+
+class CleanerServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                Console\ClearUpCommand::class
+            ]);
+        }
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__.'/config.php', 'vendor-cleaner');
+    }
+
+}
---
+++ src/Console/ClearUpCommand.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Inn20\VendorCleaner\Console;
+
+use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
+
+class ClearUpCommand  extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'vendor-cleaner';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Cleanup vendor directory.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $vendorDir = config('vendor-cleaner.dir');
+        $vendorBackupDir = config('vendor-cleaner.backup_dir');
+        $rules = config('vendor-cleaner.rules');
+
+        $filesystem = new Filesystem();
+
+        foreach ($rules as $packageDir => $rule) {
+            if (!$filesystem->exists($vendorDir . '/' . $packageDir)) {
+                continue;
+            }
+
+            $patterns = explode(' ', $rule);
+            foreach($patterns as $pattern){
+                try{
+                    $finder = new Finder();
+                    $finder->name($pattern)->in( $vendorDir . '/' . $packageDir);
+
+                    // we can't directly iterate over $finder if it lists dirs we're deleting
+                    $files = iterator_to_array($finder);
+
+                    /** @var \SplFileInfo $file */
+                    foreach($files as $file){
+                        if($file->isDir()){
+                            //$filesystem->deleteDirectory($file);
+                            $filesystem->copyDirectory($file, $vendorBackupDir);
+                            $filesystem->deleteDirectory($file);
+                        }elseif($file->isFile()){
+                            //$filesystem->delete($file);
+                            $filesystem->move($file, $vendorBackupDir);
+                        }
+                    }
+                }catch(\Exception $e){
+                    $this->error("Could not parse $packageDir ($pattern): ".$e->getMessage());
+                }
+            }
+
+        }
+
+    }
+}
